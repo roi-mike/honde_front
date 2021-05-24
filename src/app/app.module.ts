a1c65cc945400b2bdd1421aaaa815ac43518fda6
Maboza Hondi <dev.mabozahondi@gmail.com>
2021-05-24 19:39:49 +0200

    premiere version de la page home v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BarreDeNavigationComponent } from './barre-de-navigation/barre-de-navigation.component';
 import { ErreurPageComponent } from './erreur-page/erreur-page.component';
+import { HomePageViewComponent } from './home-page-view/home-page-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BarreDeNavigationComponent,
     ErreurPageComponent,
+    HomePageViewComponent,
   ],
   imports: [
     BrowserModule,
