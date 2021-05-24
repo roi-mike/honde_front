2548dc5e3bd195503741fd1c558f7ed1964bf25a
Maboza Hondi <dev.mabozahondi@gmail.com>
2021-05-25 01:54:26 +0200

    rcrtcrt

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,6 @@
 import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ErreurPageComponent } from './erreur-page/erreur-page.component';
-import { HomePageViewComponent } from './home-page-view/home-page-view.component';
-
 const routes: Routes = [
-  {
-    path: '',
-    component : HomePageViewComponent
-  },
-  {
-    path: '**',
-    component: ErreurPageComponent,
-  }
 ];
 
 @NgModule({
