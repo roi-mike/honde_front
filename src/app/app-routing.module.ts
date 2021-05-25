2d9d1d5725a7eaf54a2c014fe7614034cae0327e
Maboza Hondi <dev.mabozahondi@gmail.com>
2021-05-25 03:37:34 +0200

    cefcef

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,19 @@
 import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ErreurPageComponent } from './erreur-page/erreur-page.component';
+
+
 import { HomePageViewComponent } from './home-page-view/home-page-view.component';
+import { ErreurPageComponent } from './erreur-page/erreur-page.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component : HomePageViewComponent
-  },
-  {
-    path: '**',
-    component: ErreurPageComponent,
-  }
-];
+{
+  path: '',
+  component : HomePageViewComponent,
+},
+{
+  path: '**',
+  component : ErreurPageComponent,
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
